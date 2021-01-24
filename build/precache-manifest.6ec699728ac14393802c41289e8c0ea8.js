44299e05adfe606afa2ada84e380e4b7408fce64
Ella <exu6056@gmail.com>
2021-01-23 21:52:31 -0800

    :rocket: deploy

---
+++ build/precache-manifest.6ec699728ac14393802c41289e8c0ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1777beded774369c5cce4dc7879bd0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3646635e7ffeca37af5c",
+    "url": "/static/css/main.ea10b12a.chunk.css"
+  },
+  {
+    "revision": "fd016e693263fe0f4e8d",
+    "url": "/static/js/2.9e4bfd78.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.9e4bfd78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3646635e7ffeca37af5c",
+    "url": "/static/js/main.8123a811.chunk.js"
+  },
+  {
+    "revision": "c07e699ce0d9b7a03d8a",
+    "url": "/static/js/runtime-main.fd41696f.js"
+  }
+]);
